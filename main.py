b5e1812882392f0551a6407857779260aae47090
yanggyuheon <gyuheon0@gmail.com>
2022-11-04 21:33:33 +0900

    update division by zero

--- main.py
+++ main.py
@@ -42,7 +42,10 @@ while True:
             print(num1, "*", num2, "=", multiply(num1, num2))
 
         else:
-            print(num1, "/", num2, "=", divide(num1, num2))
+            if num2 == 0:
+                print("Divison by zero isn't possible")
+            else:
+                print(num1, "/", num2, "=", divide(num1, num2))
 
         # break the while loop if answer is no
         next_calculation = input("Let's do next calculation? (yes/no): ")
